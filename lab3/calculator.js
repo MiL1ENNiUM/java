363cc3d317359c2fb33ac771e4b5a77d94dff19e
MiL1ENNiUM <39197559+MiL1ENNiUM@users.noreply.github.com>
2018-05-11 21:50:55 +0300

    Add files via upload

---
+++ lab3/calculator.js
@@ -0,0 +1,13 @@
+var calculator = {
+    read: function () {
+        this.a = +prompt('a?', 0);
+        this.b = +prompt('b?', 0);
+    },
+    sum: function() {
+        return this.a + this.b;
+    },
+
+    mul: function() {
+        return this.a * this.b;
+    }
+}
